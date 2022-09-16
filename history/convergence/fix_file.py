1735f1654f63c24ce6614db18f0bd3d235919378
Thieu <nguyenthieu2102@gmail.com>
2022-09-16 16:32:22 +0700

    rename

---
+++ history/convergence/fix_file.py
@@ -0,0 +1,13 @@
+import os
+
+folder_list = os.listdir(".")
+
+for idx_folder, folder in enumerate(folder_list):
+    file_list = os.listdir(f"./{folder}")
+
+    for idx_file, file_old in enumerate(file_list):
+
+        if file_old.startswith("F"):
+            func_name, csv_name = file_old.split(":")
+            file_new = f"{func_name}_convergence.csv"
+            os.rename(f"./{folder}/{file_old}", f"./{folder}/{file_new}")
