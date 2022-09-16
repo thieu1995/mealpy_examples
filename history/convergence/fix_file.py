5be31c42f8e3aa607d849c4b4b4aa8254b54f1d6
Thieu <nguyenthieu2102@gmail.com>
2022-09-16 16:38:17 +0700

    Fix the name of results file

--- history/convergence/fix_file.py
+++ history/convergence/fix_file.py
@@ -3,11 +3,15 @@ import os
 folder_list = os.listdir(".")
 
 for idx_folder, folder in enumerate(folder_list):
-    file_list = os.listdir(f"./{folder}")
 
-    for idx_file, file_old in enumerate(file_list):
+    if os.path.isdir(folder):
 
-        if file_old.startswith("F"):
-            func_name, csv_name = file_old.split(":")
-            file_new = f"{func_name}_convergence.csv"
-            os.rename(f"./{folder}/{file_old}", f"./{folder}/{file_new}")
+        file_list = os.listdir(f"./{folder}")
+
+        for idx_file, file_old in enumerate(file_list):
+
+            if file_old.startswith("F"):
+                if ":" in file_old:
+                    func_name, csv_name = file_old.split(":")
+                    file_new = f"{func_name}_convergence.csv"
+                    os.rename(f"./{folder}/{file_old}", f"./{folder}/{file_new}")
